093f4199b6b875776f2a8b2511ca8430f5a59eba
xiatin <bifi112537@gmail.com>
2025-06-15 21:32:18 +0800

    6/15設立git版本管理

---
+++ script.js
@@ -0,0 +1,68 @@
+const input = document.getElementById("search_Input");
+const searchBox = document.getElementById("SearchBox");
+
+input.addEventListener("focus", () => {
+  searchBox.style.display = "block";
+});
+
+document.addEventListener("click", (event) => {
+  // 點擊的不是輸入框或 searchBox，也不是它們的子元素
+  if (
+    !input.contains(event.target) &&
+    !searchBox.contains(event.target)
+  ) {
+    searchBox.style.display = "none";
+  }
+});
+// musicCrad 圖片
+document.querySelectorAll('.MusicCrad').forEach(parent => {
+  const bg = parent.dataset.bg;
+  if (!bg) return;
+
+  // 父層背景
+  parent.style.backgroundImage = `url(${bg})`;
+
+  // 子層背景
+  const child = parent.querySelector('.MusicCrad_2');
+  const child2 = parent.querySelector('.CD');
+  if (child) {
+    child.style.backgroundImage = `url(${bg})`;
+    child.style.backgroundSize = 'cover';
+    child.style.backgroundPosition = 'center';
+    child.style.backgroundRepeat = 'no-repeat';
+    child2.style.backgroundImage = `url(${bg})`;
+    child2.style.backgroundSize = 'cover';
+    child2.style.backgroundPosition = 'center';
+    child2.style.backgroundRepeat = 'no-repeat';
+  }
+});
+// 水平滑動
+const scrollContainer = document.querySelector('#MusicCradList');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+scrollContainer.addEventListener('mousedown', (e) => {
+  isDown = true;
+  scrollContainer.classList.add('active');
+  startX = e.pageX - scrollContainer.offsetLeft;
+  scrollLeft = scrollContainer.scrollLeft;
+});
+
+scrollContainer.addEventListener('mouseleave', () => {
+  isDown = false;
+  scrollContainer.classList.remove('active');
+});
+
+scrollContainer.addEventListener('mouseup', () => {
+  isDown = false;
+  scrollContainer.classList.remove('active');
+});
+
+scrollContainer.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - scrollContainer.offsetLeft;
+  const walk = (x - startX) * 1; // 拖曳速度
+  scrollContainer.scrollLeft = scrollLeft - walk;
+});
